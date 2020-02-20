04b28693a420d5b20214afd55927b5b19e3ad65e
Igor Smirnov <piter3343@gmail.com>
2020-02-20 19:18:30 +0300

    implemented cd and ls

--- bash-cli/src/main/kotlin/ru/hse/spb/cli/commands/CdCommand.kt
+++ bash-cli/src/main/kotlin/ru/hse/spb/cli/commands/CdCommand.kt
@@ -3,6 +3,7 @@ package ru.hse.spb.cli.commands
 import ru.hse.spb.cli.Context
 import ru.hse.spb.cli.InterpreterException
 import java.io.File
+import java.nio.file.Paths
 
 class CdCommand(private val arguments: List<String>, private val context: Context) : Command {
     override fun run(input: List<String>): List<String> {
@@ -11,9 +12,7 @@ class CdCommand(private val arguments: List<String>, private val context: Contex
                 changeDirectoryTo(System.getProperty("user.home"))
             }
             arguments.size == 1 -> {
-                changeDirectoryTo(
-                    "${File("").absolutePath}${File.pathSeparator}${arguments[0]}"
-                )
+                changeDirectoryTo(context.toAbsoluteFileName(arguments[0]))
             }
             else -> {
                 throw InterpreterException("cd: too many arguments")
@@ -23,8 +22,13 @@ class CdCommand(private val arguments: List<String>, private val context: Contex
     }
 
     private fun changeDirectoryTo(newDirectoryName: String) {
-        TODO()
+        val newDirectory = File(newDirectoryName)
+        if (!newDirectory.exists()) {
+            throw InterpreterException("cd: No such file or directory")
+        }
+        if (newDirectory.isFile) {
+            throw InterpreterException("cd: Not a directory")
+        }
+        context.currentDirectory = Paths.get(newDirectory.absolutePath).normalize().toString()
     }
-
-
 }
--- bash-cli/src/main/kotlin/ru/hse/spb/cli/commands/LsCommand.kt
+++ bash-cli/src/main/kotlin/ru/hse/spb/cli/commands/LsCommand.kt
@@ -1,9 +1,32 @@
 package ru.hse.spb.cli.commands
 
 import ru.hse.spb.cli.Context
+import ru.hse.spb.cli.InterpreterException
+import java.io.File
 
 class LsCommand(private val arguments: List<String>, private val context: Context) : Command {
     override fun run(input: List<String>): List<String> {
-        TODO()
+        return when {
+            arguments.isEmpty() -> {
+                listDirectoryContents("")
+            }
+            arguments.size == 1 -> {
+                listDirectoryContents(arguments[0])
+            }
+            else -> {
+                throw InterpreterException("ls: too many arguments")
+            }
+        }
+    }
+
+    private fun listDirectoryContents(directoryShortName: String): List<String> {
+        val directory = File(context.toAbsoluteFileName(directoryShortName))
+        if (!directory.exists()) {
+            throw InterpreterException("ls: No such file or directory")
+        }
+        if (directory.isFile) {
+            return listOf(directoryShortName)
+        }
+        return directory.list()!!.toList()
     }
 }
